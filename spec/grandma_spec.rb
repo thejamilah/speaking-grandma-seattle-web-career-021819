e86f71d0acd815fbd74d965c7d05fb94b614381e
thejamilah <jamilahmcw@gmail.com>
2019-06-24 08:30:11 -0700

    placeholder...working on other things

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
     expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
@@ -12,11 +14,10 @@ describe '#speak_to_grandma' do
   end
 
   it 'responds with NO, NOT SINCE 1938! when she can hear you' do
-    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
+    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq 'NO, NOT SINCE 1938!'
   end
 
   it 'responds with NO, NOT SINCE 1938! for a second time' do
-    expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
+    expect(speak_to_grandma('WHAT?')).to eq 'NO, NOT SINCE 1938!'
   end
-
 end
