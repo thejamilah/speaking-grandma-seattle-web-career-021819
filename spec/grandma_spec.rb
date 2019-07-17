08897151c78b1b6d4ce57f40a37ae1f40fe97d28
thejamilah <jamilahmcw@gmail.com>
2019-07-17 08:58:15 -0700

    Done.

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 describe '#speak_to_grandma' do
   it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
     expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
